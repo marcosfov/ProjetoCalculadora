8662227716293a769a1a0b00db77dac717a53c50
Marcos Valeriano <marcosfov@gmail.com>
2024-07-29 16:37:01 -0300

    Projeto calculadora

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,59 @@
+﻿using CalculatorAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("Addition")]
+        public ActionResult<Calculadora> Addition(double value1, double value2)
+        {
+            var calc = new Calculadora(value1, value2);
+            calc.Value1 = value1;
+            calc.Value2 = value2;
+
+            calc.sum();
+            return Ok(calc);
+        }
+
+        [HttpGet("Subtraction")]
+        public ActionResult<Calculadora> Subtraction(double value1, double value2)
+        {
+            var calc = new Calculadora(value1, value2);
+            calc.Value1 = value1;
+            calc.Value2 = value2;
+
+            calc.subtract();
+            return Ok(calc);
+        }
+
+        [HttpGet("Multiplication")]
+        public ActionResult<Calculadora> Multiplication(double value1, double value2)
+        {
+            var calc = new Calculadora(value1, value2);
+            calc.Value1 = value1;
+            calc.Value2 = value2;
+
+            calc.multiply();
+            return Ok(calc);
+        }
+
+        [HttpGet("Division")]
+        public ActionResult<Calculadora> Division(double value1, double value2)
+        {
+            var calc = new Calculadora(value1, value2);
+            calc.Value1 = value1;
+            calc.Value2 = value2;
+
+            if (value2 < 0) { 
+                throw new DivideByZeroException();
+             };
+
+            calc.divide();
+            return Ok(calc);
+        }
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿namespace CalculatorAPI.Models
+{
+    public class Calculadora
+    {
+        public double Value1 { get; set; }
+        public double Value2 { get; set; }
+        public double Resultado { get; set; }
+
+
+
+        public Calculadora(double value1, double value2)
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+        }
+
+        public void sum()
+        {
+            Resultado = Value1 + Value2;
+        }
+
+        public void subtract()
+        {
+            Resultado = Value1 - Value2;
+        }
+
+        public void multiply()
+        {
+            Resultado = Value1 * Value2;
+        }
+
+        public void divide()
+        {
+            Resultado = Value1 / Value2;
+        }
+
+    }
+}
